d8711216921df27c63cc75c935192d7e2aa50061
Josh Hauser <33412940+joshhauser@users.noreply.github.com>
2019-09-23 16:49:49 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -61,6 +61,9 @@ int  is_cell_occupied(NumBox pos);
 void init_gamepaws_1();
 void init_gamepaws_2();
 void move_pawn(NumBox start, NumBox end);
+int  possible_moves(NumBox **poss_moves, NumBox pos, int moves);
+int  out_of_range(NumBox pos);
+void set_game_finished(int* finished);
 
 /* View */
 
@@ -70,9 +73,12 @@ void draw_unicorn(POINT origin, COULEUR color);
 void draw_paladin(POINT origin, COULEUR color);
 void erase_pawn(POINT origin);
 void draw_pawn(Box pawn, POINT origin);
-void display_menu();
-int  get_interface_choice(POINT click);
-int is_on_board(POINT click);
+void display_interface_choice();
+void display_gamemode_choice();
+int  is_on_board(POINT click);
+void highlight_possible_moves(NumBox pos, int moves, int interface);
+void display_endgame_menu(Coul color);
+
 
 /* Controller */
 
@@ -82,6 +88,10 @@ POINT  numbox_to_point_ig1(NumBox n);
 NumBox point_to_numbox_ig1(POINT p);
 POINT  numbox_to_point_ig2(NumBox n);
 NumBox point_to_numbox_ig2(POINT p);
+int  get_interface_choice(POINT click);
+int  get_gamemode_choice(POINT click);
+int  replay(POINT click);
+int  is_on_player_side(POINT click, int interface, Coul color);
 
 /* Main */
 
@@ -89,45 +99,75 @@ int main()
 {
     NumBox n1, n2;
     POINT choice, click1, click2, p1, p2;
-    int interface;
+    int interface, gamemode, finished = 0, inGame = 1, type1, type2;
 
+    Coul color;
 
     init_graphics(WIDTH, HEIGHT);
     affiche_auto_off();
-    display_menu();
 
-    choice = wait_clic();
-    interface = get_interface_choice(choice);
-    fill_screen(black);
-
-    init_gameboard();
-    init_gamepaws_2();
-    draw_gameboard(interface);
-
-    while (1)
+    while (inGame)
     {
+        color = WHITE;
+        display_interface_choice();
+        choice = wait_clic();
+        interface = get_interface_choice(choice);
+        fill_screen(black);
+
+        display_gamemode_choice();
+        choice = wait_clic();
+        gamemode = get_gamemode_choice(choice);
+        fill_screen(black);
+
+        init_gameboard();
+        init_gamepaws_2();
+        draw_gameboard(interface);
+
         do
         {
-            click1 = wait_clic();
-            click2 = wait_clic();
+            do
+            {
+                click1 = wait_clic();
+                n1 = point_to_numbox(click1, interface);
+            } while (!is_cell_occupied(n1) || !is_on_player_side(click1, interface, color));
 
-            n1 = point_to_numbox(click1, interface);
-            n2 = point_to_numbox(click2, interface);
+            type1 = gameboard[n1.y][n1.x].type;
 
+            do
+            {
+                click2 = wait_clic();
+                n2 = point_to_numbox(click2, interface);
+            } while (is_on_player_side(click2, interface, color));
+
+            type2 = gameboard[n2.y][n2.x].type;
+
+            highlight_possible_moves(n1, gameboard[n1.y][n1.x].edging, interface);
 
             p1 = numbox_to_point(n1, interface);
             p2 = numbox_to_point(n2, interface);
+            erase_pawn(p1);
+            move_pawn(n1, n2);
+            draw_pawn(gameboard[n2.y][n2.x], p2);
 
-        } while (!is_cell_occupied(n1) || !is_on_board(click1) || !is_on_board(click2));
 
-        erase_pawn(p1);
-        move_pawn(n1, n2);
-        draw_pawn(gameboard[n2.y][n2.x], p2);
+            if (type1 == PALADIN && type2 == UNICORN )
+            {
+                set_game_finished(&finished);
+            }
+            else
+            {
+                if (color == BLACK) color = WHITE;
+                else color = BLACK;
+            }
 
-        affiche_all();
+            affiche_all();
+        } while (!finished);
+
+        display_endgame_menu(color);
+        click1 = wait_clic();
+        inGame = replay(click1);
+        fill_screen(black);
     }
-
-    wait_escape();
 
     return 0;
 }
@@ -195,36 +235,137 @@ void move_pawn(NumBox start, NumBox end)
     gameboard[start.y][start.x].type = EMPTY;
 }
 
+int possible_moves(NumBox **poss_moves, NumBox pos, int moves)
+{
+    int i, j = 0;
+
+    *poss_moves = (NumBox *) malloc(sizeof(NumBox) * 4 * moves);
+    NumBox buffer;
+
+    (*poss_moves)[j++] = (NumBox) { pos.x, pos.y + moves };
+    (*poss_moves)[j++] = (NumBox) { pos.x, pos.y - moves };
+    (*poss_moves)[j++] = (NumBox) { pos.x + moves, pos.y };
+    (*poss_moves)[j++] = (NumBox) { pos.x - moves, pos.y };
+
+    for (i = 1; i != moves; i++)
+    {
+        (*poss_moves)[j++] = (NumBox) { pos.x + i, pos.y - (moves - i) };
+        (*poss_moves)[j++] = (NumBox) { pos.x - i, pos.y - (moves - i) };
+        printf("Move  : x : %d y : %d\n", (*poss_moves)[j - 2].x, (*poss_moves)[j - 2].y);
+        printf("Move2 : x : %d y : %d\n", (*poss_moves)[j - 1].x, (*poss_moves)[j - 1].y);
+    }
+
+    for (i = 0; i != j; i++)
+    {
+        if (out_of_range((*poss_moves)[i]) || is_cell_occupied((*poss_moves)[i]))
+        {
+            printf("M : x : %d y : %d\n", (*poss_moves)[i].x, (*poss_moves)[i].y);
+            buffer.x = (*poss_moves)[j - 1].x;
+            buffer.y = (*poss_moves)[j - 1].y;
+            (*poss_moves)[j - 1].x = (*poss_moves)[i].x;
+            (*poss_moves)[j - 1].y = (*poss_moves)[i].y;
+            j--;
+            (*poss_moves)[i].x = buffer.x;
+        }   (*poss_moves)[i].y = buffer.y;
+    }
+
+    return j;
+}
+
+int out_of_range(NumBox pos)
+{
+    return !(pos.x >= 0 || pos.x <= 5 || pos.y >= 0 || pos.y <= 5);
+}
+
+void set_game_finished(int* finished)
+{
+     *finished = 1;
+}
+
 /* View */
 
-void display_menu()
+void highlight_possible_moves(NumBox pos, int moves, int interface)
 {
-    POINT top, bottom, label;
+    NumBox *poss_moves = NULL;
+    int i, length = possible_moves(&poss_moves, pos, moves);
+    POINT p;
 
-    top.x = MID_WIDTH;
-    top.y = HEIGHT - MARGIN;
-    bottom.x = MID_WIDTH;
-    bottom.y = MARGIN;
-    label.x = 50;
-    label.y = MID_HEIGHT + 20;
+    for (i = 0; i != length; i++)
+    {
+        p = numbox_to_point(poss_moves[i], interface);
+        p.x += CIRCLE_RADIUS;
+        p.y += CIRCLE_RADIUS;
 
-    //printf("WIDTH : %d \ntop : %d %d\n", WIDTH, top.x, top.y);
-
-    draw_line(top, bottom, white);
-
-    aff_pol("Interface 1", 20, label, white);
-
-    label.x = MID_WIDTH + 50;
-
-    aff_pol("Interface 2", 20, label, white);
+        draw_fill_circle(p, 10, orange);
+    }
 
     affiche_all();
 }
 
-int get_interface_choice(POINT click)
+void display_interface_choice()
 {
-    if (click.x >= 0 && click.x < MID_WIDTH) return 1;
-    else return 2;
+    POINT top, bottom, label;
+    int size;
+
+    size = 30;
+    label.x = MID_WIDTH - (size * 2) - 15;
+    label.y = HEIGHT - (MARGIN / 2);
+    aff_pol("Interface", size, label, red);
+
+    top.x = MID_WIDTH;
+    top.y = label.y - (MARGIN * 2);
+    bottom.x = MID_WIDTH;
+    bottom.y = MARGIN;
+
+    draw_line(top, bottom, white);
+
+    size = 25;
+    label.x = 50;
+    label.y = MID_HEIGHT + 20;
+
+    aff_pol("Interface 1", size, label, white);
+
+    label.x = MID_WIDTH + 50;
+
+    aff_pol("Interface 2", size, label, white);
+
+    affiche_all();
+}
+
+void display_gamemode_choice()
+{
+    POINT top, bottom, label;
+    int size = 30;
+
+    label.x = MID_WIDTH - (size * 3);
+    label.y = HEIGHT - (MARGIN / 2);
+
+    aff_pol("Mode de jeu", size, label, orange);
+
+    top.x = MID_WIDTH;
+    top.y = label.y - (MARGIN * 2);
+    bottom.x = MID_WIDTH;
+    bottom.y = MARGIN;
+    draw_line(top, bottom, white);
+
+    size = 25;
+    label.x = ((MID_WIDTH / 2) / 2) - size;
+    label.y = MID_HEIGHT + (size * 2);
+    aff_pol("Joueur", size, label, white);
+    label.y = label.y - size - (size / 4);
+    aff_pol("contre", size, label, white);
+    label.y = label.y - size - (size / 4);
+    aff_pol("joueur", size, label, white);
+
+    label.x = MID_WIDTH + label.x;
+    label.y = MID_HEIGHT + (size * 2 );
+    aff_pol("Joueur", size, label, white);
+    label.y = label.y - size - (size / 4);
+    aff_pol("contre", size, label, white);
+    label.x = label.x + size;
+    label.y = label.y - size - (size / 4);
+    aff_pol("IA", size, label, white);
+    affiche_all();
 }
 
 void draw_unicorn(POINT origin, COULEUR color)
@@ -313,8 +454,42 @@ void erase_pawn(POINT origin)
    draw_unicorn(origin, black);
 }
 
-int is_on_board(POINT click){
+int is_on_board(POINT click)
+{
     return (click.x >= MARGIN && click.x <= (WIDTH - MARGIN) && click.y >= MARGIN  && click.y <= (HEIGHT - MARGIN));
+}
+
+
+void display_endgame_menu(Coul color)
+{
+    fill_screen(black);
+
+    POINT top, bottom, label;
+    int size = 30;
+    label.x = MARGIN + (size / 2);
+    label.y = HEIGHT - (MARGIN / 2);
+
+    if (color == 0) aff_pol("Le joueur noir gagne !", size, label, skyblue);
+    else aff_pol("Le joueur blanc gagne !", size, label, skyblue);
+
+    top.x = MID_WIDTH;
+    top.y = label.y - (MARGIN * 2);
+    bottom.x = MID_WIDTH;
+    bottom.y = MARGIN;
+
+    draw_line(top, bottom, white);
+
+    size = 25;
+    label.x = 50;
+    label.y = MID_HEIGHT + 20;
+
+    aff_pol("Rejouer", size, label, white);
+
+    label.x = MID_WIDTH + 50;
+
+    aff_pol("Quitter", size, label, white);
+
+    affiche_all();
 }
 
 /* Controller */
@@ -370,3 +545,29 @@ NumBox point_to_numbox_ig2(POINT p)
 
     return n;
 }
+
+int get_interface_choice(POINT click)
+{
+    if (click.x >= 0 && click.x < MID_WIDTH) return 1;
+    else return 2;
+}
+
+int get_gamemode_choice(POINT click)
+{
+    if (click.x >= 0 && click.x < MID_WIDTH) return 1;
+    else return 2;
+}
+
+int is_on_player_side(POINT click, int interface, Coul color)
+{
+    NumBox cell = point_to_numbox(click, interface);
+
+    return (is_on_board(click) && (gameboard[cell.y][cell.x].type == PALADIN || gameboard[cell.y][cell.x].type == UNICORN ) && gameboard[cell.y][cell.x].color == color);
+}
+
+int replay(POINT click)
+{
+    if (click.y >= 0 && click.x < MID_WIDTH) return 1;
+    else return 0;
+}
+
